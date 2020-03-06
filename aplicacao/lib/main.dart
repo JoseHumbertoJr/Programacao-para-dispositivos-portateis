4de7c272cf0de40ab47de059c8f7ea7e35330b99
JoseHumbertoJr <31288586+JoseHumbertoJr@users.noreply.github.com>
2020-03-06 18:29:59 -0300

    Add files via upload

---
+++ aplicacao/lib/main.dart
@@ -0,0 +1,31 @@
+
+import 'package:flutter/material.dart';
+
+
+void main() => runApp(
+  Center(
+    child: Text(
+      'Olá Mundo!!!',
+      textDirection: TextDirection.ltr,
+      ),
+  ),
+);
+
+/*HELLO WORD 2*/
+/*
+main() => runApp(
+  Directionality(
+    textDirection: TextDirection.ltr,
+    child: Container(
+      color: Color(0xFF444444),
+      child: Center(
+        child: Text(
+          'Olá Mundo!!!',style: TextStyle(color: Color(0xFFFD620A),
+          fontSize: 32.0,
+          ),
+        ),
+      ),
+    ),
+  ),
+);
+*/
